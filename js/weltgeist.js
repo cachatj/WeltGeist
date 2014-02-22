7fe765afc344507442091de7ad072c28d2bfd552
sos0 <so.c.choi@gmail.com>
2014-02-22 00:39:20 -0800

    adding folders

---
+++ js/weltgeist.js
@@ -0,0 +1,22 @@
+var data = {
+	{	
+	},
+	{
+	},
+	{
+	},
+	{
+	},
+	{
+	},
+
+}
+
+createStoryJS({
+  type:       'timeline',
+  width:      '100%',
+  height:     '600',
+  start_zoom_adjust:  2,
+  source:     timeline,
+  embed_id:   'visualization'
+});
